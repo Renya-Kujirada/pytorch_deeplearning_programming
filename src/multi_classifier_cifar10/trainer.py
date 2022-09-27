dca4ca152f86c6b645f470ab0acba4a8f8345af5
renya <rsmilek5492@gmail.com>
2022-09-27 13:09:26 +0000

    added test method process.

--- src/multi_classifier_cifar10/trainer.py
+++ src/multi_classifier_cifar10/trainer.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 from tqdm import tqdm
 
@@ -7,24 +8,24 @@ class Trainer:
         pass
 
     # training loop
-    def fit(self, net, optimizer, criterion, num_epoch, train_loader, test_loader, device, history):
+    def fit(self, net, optimizer, criterion, num_epochs, train_loader, test_loader, device, history):
         base_epochs = len(history)  # 追加学習用
-        for epoch in range(base_epochs, num_epoch+base_epochs):
-            self.train(net, optimizer, criterion, epoch, train_loader, device)
-            self.test(net, optimizer, criterion, epoch, test_loader, device)
+        for epoch in range(base_epochs, num_epochs+base_epochs):
+            train_loss, train_acc = self.train(net, optimizer, criterion, epoch, train_loader, device)
+            val_loss, val_acc = self.test(net, optimizer, criterion, epoch, test_loader, device)
+            item = np.array([epoch+1, train_loss, train_acc, val_loss, val_acc])
+            history = np.vstack((history, item))
+        return history
 
     # training in one epoch
     def train(self, net, optimizer, criterion, epoch, train_loader, device):
         net.train()
-
         train_loss = 0
         train_acc = 0
-        val_loss = 0
-        val_acc = 0
         count = 0
 
         # バッチ学習
-        for inputs, label in tqdm(train_loader):
+        for inputs, labels in tqdm(train_loader):
             count += len(labels)
             inputs = inputs.to(device)
             labels = labels.to(device)
@@ -36,14 +37,14 @@ class Trainer:
             outputs = net(inputs)
 
             # 損失計算
-            loss = criterion(outpurs, labels)
+            loss = criterion(outputs, labels)
             train_loss += loss.item()
 
             # 勾配計算
             loss.backward()
 
             # パラメータ修正
-            optimiser.step()
+            optimizer.step()
 
             # 予測値算出*
             predicted = torch.max(outputs, 1)[1]
@@ -55,10 +56,40 @@ class Trainer:
             avg_train_loss = train_loss / count
             avg_train_acc = train_acc / count
 
-        print(f"Epoch: {epoch}, loss: {avg_train_loss}")
+        print(f"Epoch: {epoch+1}, loss: {avg_train_loss:.5f}, acc: {avg_train_acc:.5f}")
+        return avg_train_loss, avg_train_acc
 
     def test(self, net, optimizer, criterion, epoch, test_loader, device):
-        pass
+        net.eval()
+        val_loss = 0
+        val_acc = 0
+        count = 0
+
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                count += len(labels)
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                # 予測計算
+                outputs = net(inputs)
+
+                # 損失計算
+                loss = criterion(outputs, labels)
+                val_loss += loss.item()  # sum up batch loss
+
+                # 予測値算出
+                predicted = torch.max(outputs, 1)[1]
+
+                # 正解件数算出
+                val_acc += (predicted == labels).sum().item()
+
+                # 損失と精度の計算
+                avg_val_loss = val_loss / count
+                avg_val_acc = val_acc / count
+
+        print(f"Epoch: {epoch+1}, val_loss: {avg_val_loss:.5f}, val_acc: {avg_val_acc:.5f}")
+        return avg_val_loss, avg_val_acc
 
 
 """
