86c6a194c06e9d9ec16f4a9e59846928b6fefd66
renya <rsmilek5492@gmail.com>
2022-10-04 14:32:38 +0000

    added structure of conv layer.

--- src/model_tuning/model.py
+++ src/model_tuning/model.py
@@ -6,3 +6,14 @@ class CNN(nn.Module):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 32, 3, padding=(1, 1))  # 入力チャネル数，出力チャネル数，カーネルサイズ
         self.conv2 = nn.Conv2d(32, 32, 3, padding=(1, 1))  # padding: 入力テンソルの外側を(1,1)の大きさのダミーデータで埋める．
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=(1, 1))
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=(1, 1))
+        self.conv5 = nn.Conv2d(64, 128, 3, padding=(1, 1))
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=(1, 1))
+
+        self.relu = nn.ReLU(inplace=True)
+        self.flatten = nn.Flatten()
+        self.maxpool = nn.MaxPool2d((2, 2))
+
+        self.l1 = nn.Linear(4*4*128, 128)
+        self.l2 = nn.Linear(128, num_classes)
