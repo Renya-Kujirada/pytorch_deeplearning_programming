6dc947cddd43598fefa199d39eb5d8b4ba87354d
renya <rsmilek5492@gmail.com>
2022-10-11 13:15:36 +0000

    created new dropout model.

--- src/model_tuning/model.py
+++ src/model_tuning/model.py
@@ -48,3 +48,59 @@ class CNN(nn.Module):
         x2 = self.flatten(x1)
         x3 = self.classifier(x2)
         return x3
+
+
+class CNN_with_Dropout(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 32, 3, padding=(1, 1))  # 入力チャネル数，出力チャネル数，カーネルサイズ
+        self.conv2 = nn.Conv2d(32, 32, 3, padding=(1, 1))  # padding: 入力テンソルの外側を(1,1)の大きさのダミーデータで埋める．
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=(1, 1))
+        self.conv4 = nn.Conv2d(64, 64, 3, padding=(1, 1))
+        self.conv5 = nn.Conv2d(64, 128, 3, padding=(1, 1))
+        self.conv6 = nn.Conv2d(128, 128, 3, padding=(1, 1))
+
+        self.relu = nn.ReLU(inplace=True)
+        self.flatten = nn.Flatten()
+        self.maxpool = nn.MaxPool2d((2, 2))
+
+        self.l1 = nn.Linear(4*4*128, 128)
+        self.l2 = nn.Linear(128, num_classes)
+
+        self.dropout1 = nn.Dropout(0.2)
+        self.dropout2 = nn.Dropout(0.3)
+        self.dropout3 = nn.Dropout(0.4)
+
+        self.features = nn.Sequential(
+            self.conv1,
+            self.relu,
+            self.conv2,
+            self.relu,
+            self.maxpool,
+            self.dropout1,
+            self.conv3,
+            self.relu,
+            self.conv4,
+            self.relu,
+            self.maxpool,
+            self.dropout2,
+            self.conv5,
+            self.relu,
+            self.conv6,
+            self.relu,
+            self.maxpool,
+            self.dropout3,
+        )
+
+        self.classifier = nn.Sequential(
+            self.l1,
+            self.relu,
+            self.dropout3,
+            self.l2,
+        )
+
+    def forward(self, x):
+        x1 = self.features(x)
+        x2 = self.flatten(x1)
+        x3 = self.classifier(x2)
+        return x3
